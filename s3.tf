a5a3abafdf25c48eeba28ef9fefe95cd528e6597
Vitali <gimvit@gmail.com>
2025-06-11 16:12:11 +0200

    feat: create bucket

---
+++ s3.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.bucket_name
+  force_destroy = true
+
+  
+  tags = {
+    Name = "Terraform State"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS region for devops"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket for storing Terraform state"
+  type        = string
+  default = "rs-devops-terrafrom-state"
+}
