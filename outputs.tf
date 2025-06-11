bf5580fec4b051ed4d30be91c2b8c7ab9115d595
Vitali <gimvit@gmail.com>
2025-06-11 23:53:45 +0200

    fix: remove unuseful outputs

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  description = "Name of the S3 bucket used for Terraform state"
-  value       = aws_s3_bucket.terraform_state.bucket
-}
-
-output "github_role" {
-  description = "Arn of the github actions role"
-  value       = aws_iam_role.github_actions.arn
-}
