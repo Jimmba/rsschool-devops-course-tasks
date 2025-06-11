8803d0cdc61267389b35aba9c6da2e6a22e77079
Vitali <gimvit@gmail.com>
2025-06-11 23:52:01 +0200

    feat: add backend

--- s3.tf
+++ s3.tf
@@ -1,9 +1,25 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket        = var.bucket_name
-  force_destroy = true
-
-
-  tags = {
-    Name = "Terraform State"
+terraform {
+  backend "s3" {
+    bucket = "rs-devops-terrafrom-state"
+    key    = "terraform.tfstate"
+    region = "eu-west-1"
   }
 }
+
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket        = var.bucket_name
+#   force_destroy = true
+
+
+#   tags = {
+#     Name = "Terraform State"
+#   }
+# }
+
+# resource "aws_s3_bucket_versioning" "state_versioning" {
+#   bucket = aws_s3_bucket.terraform_state.id
+
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
