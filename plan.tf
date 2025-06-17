415852573dd00ace509f356e96789d8a4315eddf
Vitali <gimvit@gmail.com>
2025-06-17 14:36:34 +0200

    feat: add modules

---
+++ plan.tf
@@ -0,0 +1,3 @@
+module "task1-s3" {
+  source = "./task1-s3"
+}
--- task1-s3/iam-roles.tf
+++ task1-s3/iam-roles.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_role" "github_actions" {
-  name               = "GithubActionsRole"
+  name               = "GithubActionsRole-${terraform.workspace}"
   assume_role_policy = data.aws_iam_policy_document.github_actions_role.json
 }
