0363743d2bba5e950f2666a276bb1a47ceee54c5
Vitali <gimvit@gmail.com>
2025-06-28 22:28:58 +0200

    feat: deploy k3s cluster

--- task3-k3s/ec2.tf
+++ task3-k3s/ec2.tf
@@ -27,35 +27,6 @@ resource "aws_instance" "bastion" {
     Name = "devops-${terraform.workspace}-ec2-public-1 (bastion)"
   }
 
-  provisioner "file" {
-    content     = tls_private_key.k3s-key.private_key_pem
-    destination = "/home/ubuntu/k3s.pem"
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      host        = self.public_ip
-      private_key = tls_private_key.bastion_key.private_key_pem
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod 600 /home/ubuntu/k3s.pem",
-      "sudo apt-get update -y",
-      "sudo apt-get install -y curl",
-      "curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl",
-      "chmod +x ./kubectl",
-      "sudo mv ./kubectl /usr/local/bin/kubectl"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      host        = self.public_ip
-      private_key = tls_private_key.bastion_key.private_key_pem
-    }
-  }
 }
 
 # resource "aws_instance" "public_2" {
@@ -70,6 +41,8 @@ resource "aws_instance" "bastion" {
 #   }
 # }
 
+
+
 resource "aws_instance" "private_1" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
@@ -81,25 +54,137 @@ resource "aws_instance" "private_1" {
     Name = "devops-${terraform.workspace}-ec2-private-1"
   }
 
-  user_data = <<-EOF
-              #!/bin/bash
-              curl -sfL https://get.k3s.io | sh -
-              echo "Waiting for K3s to be ready..."
-              until sudo /usr/local/bin/k3s kubectl get nodes &>/dev/null; do sleep 5; done
-
-              echo "Applying test pod..."
-              sudo /usr/local/bin/k3s kubectl apply -f https://k8s.io/examples/pods/simple-pod.yaml
-              EOF
+  
 }
 
-resource "null_resource" "get_k3s_token" {
-  depends_on = [aws_instance.private_1]
+
+resource "aws_instance" "private_2" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  subnet_id              = var.private_subnets[1].id
+  vpc_security_group_ids = [var.private-sg.id]
+  key_name               = aws_key_pair.k3s-key.key_name
+
+  tags = {
+    Name = "devops-${terraform.workspace}-ec2-private-2"
+  }
+}
+
+resource "null_resource" "wait_for_bastion_ssh" {
+  depends_on = [aws_instance.bastion]
 
   provisioner "remote-exec" {
     inline = [
-      "echo 'Waiting for node-token...'",
-      "until sudo test -f /var/lib/rancher/k3s/server/node-token; do sleep 5; done",
-      "sudo cat /var/lib/rancher/k3s/server/node-token"
+      "while [ ! -f /var/lib/cloud/instance/boot-finished ]; do echo 'Waiting for cloud-init...'; sleep 5; done",
+      "echo 'Bastion is fully ready'"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = aws_instance.bastion.public_ip
+      private_key = tls_private_key.bastion_key.private_key_pem
+      timeout     = "5m"
+    }
+  }
+
+  triggers = {
+    instance_id = aws_instance.bastion.id
+  }
+}
+
+resource "null_resource" "install_k3s_key_on_bastion" {
+  depends_on = [null_resource.wait_for_bastion_ssh]
+  provisioner "file" {
+    content     = tls_private_key.k3s-key.private_key_pem
+    destination = "/home/ubuntu/k3s.pem"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = aws_instance.bastion.public_ip
+      private_key = tls_private_key.bastion_key.private_key_pem
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod 600 /home/ubuntu/k3s.pem",
+     ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = aws_instance.bastion.public_ip
+      private_key = tls_private_key.bastion_key.private_key_pem
+    }
+  }
+
+  triggers = {
+    instance_id = aws_instance.bastion.id
+  }
+}
+
+resource "null_resource" "wait_for_private_1_ssh" {
+  depends_on = [null_resource.install_k3s_key_on_bastion, aws_instance.private_1]
+
+  provisioner "remote-exec" {
+    inline = [
+      "while [ ! -f /var/lib/cloud/instance/boot-finished ]; do echo 'Waiting for cloud-init...'; sleep 5; done",
+      "echo 'Private_1 is fully ready'"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = aws_instance.private_1.private_ip
+      private_key = tls_private_key.k3s-key.private_key_pem
+      bastion_host = aws_instance.bastion.public_ip
+      bastion_user = "ubuntu"
+      bastion_private_key = tls_private_key.bastion_key.private_key_pem
+      timeout     = "5m"
+    }
+  }
+
+  triggers = {
+    instance_id = aws_instance.private_1.id
+  }
+}
+
+resource "null_resource" "wait_for_private_2_ssh" {
+  depends_on = [null_resource.install_k3s_key_on_bastion, aws_instance.private_2]
+
+  provisioner "remote-exec" {
+    inline = [
+     "while [ ! -f /var/lib/cloud/instance/boot-finished ]; do echo 'Waiting for cloud-init...'; sleep 5; done",
+      "echo 'Private_2 is fully ready'"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = aws_instance.private_2.private_ip
+      private_key = tls_private_key.k3s-key.private_key_pem
+      bastion_host = aws_instance.bastion.public_ip
+      bastion_user = "ubuntu"
+      bastion_private_key = tls_private_key.bastion_key.private_key_pem
+      timeout     = "5m"
+    }
+  }
+
+  triggers = {
+    instance_id = aws_instance.private_2.id
+  }
+}
+
+resource "null_resource" "install_k3s_server_on_private_1" {
+  depends_on = [ null_resource.wait_for_private_1_ssh ]
+
+  provisioner "remote-exec" {
+    inline = [      
+      "curl -sfL https://get.k3s.io | sh -",
+      "echo `Waiting for K3s to be ready...`",
+      "until sudo /usr/local/bin/k3s kubectl get nodes &>/dev/null; do sleep 5; done",
     ]
 
     connection {
@@ -113,33 +198,205 @@ resource "null_resource" "get_k3s_token" {
     }
   }
 
+  triggers = {
+    instance_id = aws_instance.private_1.id
+  }
+}
+
+resource "null_resource" "copy_config" {
+  depends_on = [null_resource.install_k3s_server_on_private_1]
+
+  provisioner "remote-exec" {
+    inline = [
+      "until [ -f /etc/rancher/k3s/k3s.yaml ]; do echo 'Waiting for k3s.yaml...'; sleep 2; done",
+      "sudo cp /etc/rancher/k3s/k3s.yaml /home/ubuntu/config",
+      "sudo chown ubuntu:ubuntu /home/ubuntu/config"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = aws_instance.private_1.private_ip
+      private_key = tls_private_key.k3s-key.private_key_pem
+      bastion_host = aws_instance.bastion.public_ip
+      bastion_user = "ubuntu"
+      bastion_private_key = tls_private_key.bastion_key.private_key_pem
+    }
+  }
+
+   triggers = {
+    instance_id = aws_instance.private_1.id
+  }
+}
+
+resource "null_resource" "download_k3s_config_to_bastion_direct" {
+  depends_on = [null_resource.copy_config]
+
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p /home/ubuntu/.kube",
+
+      "scp -o StrictHostKeyChecking=no -i /home/ubuntu/k3s.pem ubuntu@${aws_instance.private_1.private_ip}:/home/ubuntu/config /home/ubuntu/config",
+      "cp /home/ubuntu/config /home/ubuntu/.kube/config",
+      # "chown ubuntu:ubuntu /home/ubuntu/.kube/config",
+      "chmod 600 /home/ubuntu/.kube/config",
+      "chown -R ubuntu:ubuntu /home/ubuntu/.kube",
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = aws_instance.bastion.public_ip
+      private_key = tls_private_key.bastion_key.private_key_pem
+    }
+  }
+
+  triggers = {
+    instance_id = aws_instance.bastion.id
+  }
+}
+
+
+resource "null_resource" "download_token" {
+  depends_on = [ null_resource.install_k3s_server_on_private_1 ]
   provisioner "local-exec" {
     command = <<EOT
       ssh -o StrictHostKeyChecking=no -i keys/bastion.pem -A ubuntu@${aws_instance.bastion.public_ip} 'ssh -o StrictHostKeyChecking=no -i k3s.pem ubuntu@${aws_instance.private_1.private_ip} sudo cat /var/lib/rancher/k3s/server/node-token' > keys/k3s_token.txt
       EOT
   }
+
+  triggers = {
+    instance_id = aws_instance.private_1.id
+  }
 }
 
+# resource "null_resource" "copy_token" {
+#   depends_on = [ null_resource.get_token_and_config_from_server ]
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo cp /etc/rancher/k3s/k3s.yaml /home/ubuntu/config",
+#       "sudo chown ubuntu:ubuntu /home/ubuntu/config"
+#     ]
+#    connection {
+#       type        = "ssh"
+#       user        = "ubuntu"
+#       host        = aws_instance.private_1.private_ip
+#       private_key = tls_private_key.k3s-key.private_key_pem
+#       bastion_host = aws_instance.bastion.public_ip
+#       bastion_user = "ubuntu"
+#       bastion_private_key = tls_private_key.bastion_key.private_key_pem
+#     }
+#   }
+
+#   provisioner "remote-exec" {
+#     inline = [
+#       "scp -i k3s.pem ubuntu@${aws_instance.private_1}:/home/ubuntu/config ./config",
+#       "sudo chown ubuntu:ubuntu /home/ubuntu/config"
+#     ]
+#    connection {
+#       type        = "ssh"
+#       user        = "ubuntu"
+#       host        = aws_instance.bastion.public_ip
+#       private_key = tls_private_key.bastion_key.private_key_pem
+#     }
+#   }
+# }
+
 data "local_file" "k3s_token" {
-  depends_on = [null_resource.get_k3s_token]
+  depends_on = [null_resource.download_token]
   filename   = "./keys/k3s_token.txt"
 }
 
-resource "aws_instance" "private_2" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.instance_type
-  subnet_id              = var.private_subnets[1].id
-  vpc_security_group_ids = [var.private-sg.id]
-  key_name               = aws_key_pair.k3s-key.key_name
+# data "local_file" "config" {
+#   depends_on = [null_resource.download_token_and_config_to_local]
+#   filename   = "./keys/config"
+# }
 
-  tags = {
-    Name = "devops-${terraform.workspace}-ec2-private-2"
+# resource "null_resource" "upload_config_to_bastion" {
+#   depends_on = [ data.local_file.config ]
+
+#    provisioner "file" {
+#     source      = "./keys/config"
+#     destination = "/home/ubuntu/.kube/config"
+
+#     connection {
+#       host        = aws_instance.bastion.public_ip
+#       user        = "ubuntu"
+#       private_key = tls_private_key.bastion_key.private_key_pem
+#     }
+#   }
+
+#   triggers = {
+#     instance_id = aws_instance.private_1.id
+#   }
+# }
+
+resource "null_resource" "install_worker_on_private_2" {
+  depends_on = [ null_resource.wait_for_private_2_ssh, data.local_file.k3s_token ]
+
+  provisioner "remote-exec" {
+    inline = [
+      "curl -sfL https://get.k3s.io | K3S_URL=https://${aws_instance.private_1.private_ip}:6443 K3S_TOKEN=${trimspace(data.local_file.k3s_token.content)} sh -"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = aws_instance.private_2.private_ip
+      private_key = tls_private_key.k3s-key.private_key_pem
+      bastion_host = aws_instance.bastion.public_ip
+      bastion_user = "ubuntu"
+      bastion_private_key = tls_private_key.bastion_key.private_key_pem
+    }
   }
 
-  depends_on = [data.local_file.k3s_token]
+  triggers = {
+    instance_id = aws_instance.private_2.id
+  }
+}
 
-  user_data = <<-EOF
-              #!/bin/bash
-              curl -sfL https://get.k3s.io | K3S_URL="https://${aws_instance.private_1.private_ip}:6443" K3S_TOKEN="${trimspace(data.local_file.k3s_token.content)}" sh -
-              EOF
+resource "null_resource" "install_kubectl_on_bastion" {
+  depends_on = [null_resource.download_k3s_config_to_bastion_direct]
+  provisioner "remote-exec" {
+    inline = [
+      "curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl",
+      "chmod +x ./kubectl",
+      "sudo mv ./kubectl /usr/local/bin/kubectl",
+      "sed -i s/127.0.0.1/${aws_instance.private_1.private_ip}/ /home/ubuntu/.kube/config"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = aws_instance.bastion.public_ip
+      private_key = tls_private_key.bastion_key.private_key_pem
+    }
+  }
+
+  triggers = {
+    instance_id = aws_instance.bastion.id
+  }
+}
+
+resource "null_resource" "pox_nginx" {
+  depends_on = [ null_resource.install_worker_on_private_2 ]
+  provisioner "remote-exec" {
+    inline = [
+      "sudo k3s kubectl apply -f https://k8s.io/examples/pods/simple-pod.yaml"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = aws_instance.private_1.private_ip
+      private_key = tls_private_key.k3s-key.private_key_pem
+      bastion_host = aws_instance.bastion.public_ip
+      bastion_user = "ubuntu"
+      bastion_private_key = tls_private_key.bastion_key.private_key_pem
+    }
+  }
+
+  triggers = {
+    instance_id = aws_instance.private_2.id
+  }
 }
