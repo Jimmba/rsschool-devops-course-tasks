cb8121f275ab5e351a77657d2a6b1b8f20f9affa
Vitali <gimvit@gmail.com>
2025-07-11 14:07:59 +0200

    feat: auto generate token

--- plan.tf
+++ plan.tf
@@ -9,10 +9,10 @@ module "task2-ec2" {
 }
 
 module "task3-k3s" {
-  source          = "./task3-k3s"
-  bastion_key     = module.task2-ec2.bastion_key
-  k3s_key         = module.task2-ec2.k3s_key
-  bastion         = module.task2-ec2.bastion
-  private_1       = module.task2-ec2.private_1
-  private_2       = module.task2-ec2.private_2
+  source      = "./task3-k3s"
+  bastion_key = module.task2-ec2.bastion_key
+  k3s_key     = module.task2-ec2.k3s_key
+  bastion     = module.task2-ec2.bastion
+  private_1   = module.task2-ec2.private_1
+  private_2   = module.task2-ec2.private_2
 }
